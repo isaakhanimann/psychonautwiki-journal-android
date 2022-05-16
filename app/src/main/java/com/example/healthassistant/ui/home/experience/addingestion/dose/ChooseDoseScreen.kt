e3704cc4e03911c362c989b4fec9422d3b2e9645
Isaak <isaak.hanimann@gmail.com>
2022-05-16 17:00:09 +0200

    Set up choose dose screen

--- app/src/main/java/com/example/healthassistant/ui/home/experience/addingestion/dose/ChooseDoseScreen.kt
+++ app/src/main/java/com/example/healthassistant/ui/home/experience/addingestion/dose/ChooseDoseScreen.kt
@@ -1,49 +1,102 @@
 package com.example.healthassistant.ui.home.experience.addingestion.dose
 
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.healthassistant.data.substances.AdministrationRoute
-import com.example.healthassistant.data.substances.Substance
+import com.example.healthassistant.data.substances.RoaDose
 import com.example.healthassistant.ui.main.routers.navigateToChooseColor
-import com.example.healthassistant.ui.previewproviders.SubstancePreviewProvider
+import com.example.healthassistant.ui.previewproviders.RoaDosePreviewProvider
 
 @Composable
 fun ChooseDoseScreen(
     navController: NavController,
     viewModel: ChooseDoseViewModel = hiltViewModel()
 ) {
-    viewModel.substance?.let {
-        ChooseDoseScreenContent(substance = it, navigateToNext = {
-            navController.navigateToChooseColor(
-                substanceName = it.name,
-                administrationRoute = AdministrationRoute.ORAL,
-                units = "mg",
-                isEstimate = false,
-                dose = 5.0,
-                experienceId = 1
-            )
-        })
+    viewModel.substance?.let { sub ->
+        ChooseDoseScreenContent(
+            roaDose = viewModel.roaDose,
+            doseText = viewModel.doseText,
+            onChangeDoseText = {
+                viewModel.doseText = it
+            },
+            isValidDose = viewModel.isValidDose,
+            isEstimate = viewModel.isEstimate,
+            onChangeIsEstimate = {
+                viewModel.isEstimate = it
+            },
+            navigateToNext = {
+                navController.navigateToChooseColor(
+                    substanceName = sub.name,
+                    administrationRoute = AdministrationRoute.ORAL,
+                    units = viewModel.roaDose?.units ?: "",
+                    isEstimate = viewModel.isEstimate,
+                    dose = viewModel.dose,
+                    experienceId = viewModel.experienceId
+                )
+            })
     }
 }
 
 @Preview
 @Composable
 fun ChooseDoseScreenContent(
-    @PreviewParameter(SubstancePreviewProvider::class) substance: Substance,
-    navigateToNext: () -> Unit = {}
+    @PreviewParameter(RoaDosePreviewProvider::class) roaDose: RoaDose?,
+    doseText: String = "5",
+    onChangeDoseText: (String) -> Unit = {},
+    isValidDose: Boolean = true,
+    isEstimate: Boolean = false,
+    onChangeIsEstimate: (Boolean) -> Unit = {},
+    navigateToNext: () -> Unit = {},
 ) {
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "Choose Dose") }) }
     ) {
-        Button(onClick = navigateToNext) {
-            Text("Next")
+        val focusManager = LocalFocusManager.current
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Column {
+                OutlinedTextField(
+                    value = doseText,
+                    onValueChange = onChangeDoseText,
+                    textStyle = MaterialTheme.typography.h3,
+                    label = { Text("Enter Dose") },
+                    isError = !isValidDose,
+                    trailingIcon = {
+                        Text(text = roaDose?.units ?: "", style = MaterialTheme.typography.h3)
+                    },
+                    keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(checked = isEstimate, onCheckedChange = onChangeIsEstimate)
+                    Text("Is Estimate")
+                }
+            }
+            Button(
+                onClick = navigateToNext,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
+            ) {
+                Text("Next", style = MaterialTheme.typography.h3)
+            }
         }
     }
 }
--- app/src/main/java/com/example/healthassistant/ui/home/experience/addingestion/dose/ChooseDoseViewModel.kt
+++ app/src/main/java/com/example/healthassistant/ui/home/experience/addingestion/dose/ChooseDoseViewModel.kt
@@ -3,9 +3,11 @@ package com.example.healthassistant.ui.home.experience.addingestion.dose
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.healthassistant.data.substances.AdministrationRoute
+import com.example.healthassistant.data.substances.RoaDose
 import com.example.healthassistant.data.substances.Substance
 import com.example.healthassistant.data.substances.repositories.SubstanceRepository
 import com.example.healthassistant.ui.main.routers.ADMINISTRATION_ROUTE_KEY
+import com.example.healthassistant.ui.main.routers.EXPERIENCE_ID_KEY
 import com.example.healthassistant.ui.main.routers.SUBSTANCE_NAME_KEY
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -17,6 +19,13 @@ class ChooseDoseViewModel @Inject constructor(
 ) : ViewModel() {
     val substance: Substance?
     val administrationRoute: AdministrationRoute?
+    val experienceId = state.get<String>(EXPERIENCE_ID_KEY)?.toInt()
+    val roaDose: RoaDose?
+    var isEstimate: Boolean = false
+    var doseText = ""
+    val dose: Double? get() = doseText.toDoubleOrNull()
+    val isValidDose: Boolean get() = dose != null
+
 
     init {
         substance = repository.getSubstance(state.get<String>(SUBSTANCE_NAME_KEY) ?: "LSD")
@@ -28,6 +37,7 @@ class ChooseDoseViewModel @Inject constructor(
         }
         assert(substance != null)
         assert(administrationRoute != null)
+        roaDose = substance?.roas?.firstOrNull { it.route == administrationRoute }?.roaDose
     }
 
 }
