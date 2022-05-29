6de2f7b147c3438dc6a073556022c2cef59fc77e
Isaak <isaak.hanimann@gmail.com>
2022-05-29 21:39:53 +0200

    Refactor timeline

--- app/src/main/java/com/example/healthassistant/ui/home/experience/timeline/AllTimelines.kt
+++ app/src/main/java/com/example/healthassistant/ui/home/experience/timeline/AllTimelines.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.graphics.nativeCanvas
@@ -24,6 +25,7 @@ import com.example.healthassistant.data.substances.RoaDuration
 import com.example.healthassistant.ui.previewproviders.TimelinesPreviewProvider
 import kotlinx.coroutines.delay
 import java.util.*
+import kotlin.time.Duration
 
 
 @Preview(showBackground = true)
@@ -80,76 +82,126 @@ fun AllTimelines(
             inset(left = 0f, top = 0f, right = 0f, bottom = 50f) {
                 val canvasHeightOuter = size.height
                 model.ingestionDrawables.forEach { ingestionDrawable ->
-                    val color = ingestionDrawable.color.getComposeColor(isDarkTheme)
-                    val startX =
-                        ingestionDrawable.ingestionPointDistanceFromStart.inWholeSeconds * pixelsPerSec
-                    val verticalInsetForLine = strokeWidth / 2
-                    inset(vertical = verticalInsetForLine) {
-                        val canvasHeightInner = size.height
-                        drawCircle(
-                            color = color,
-                            radius = 10f,
-                            center = Offset(x = startX, y = canvasHeightInner)
-                        )
-                    }
-                    ingestionDrawable.timelineDrawable?.let { timelineDrawable ->
-                        inset(vertical = verticalInsetForLine) {
-                            val canvasHeightInner = size.height
-                            drawPath(
-                                path = timelineDrawable.getStrokePath(
-                                    pixelsPerSec = pixelsPerSec,
-                                    height = canvasHeightInner,
-                                    startX = startX
-                                ),
-                                color = color,
-                                style = Stroke(
-                                    width = strokeWidth,
-                                    cap = StrokeCap.Round,
-                                    pathEffect = if (timelineDrawable.isDotted) PathEffect.dashPathEffect(
-                                        floatArrayOf(20f, 30f)
-                                    ) else null
-                                )
-                            )
-                        }
-                        drawPath(
-                            path = timelineDrawable.getFillPath(
-                                pixelsPerSec = pixelsPerSec,
-                                height = canvasHeightOuter,
-                                startX = startX
-                            ),
-                            color = color.copy(alpha = 0.1f)
-                        )
-                    }
-                }
-                val endTime = Date(model.startTime.time + model.width.inWholeMilliseconds)
-                if (model.startTime.before(currentTime) && endTime.after(currentTime)) {
-                    val timeStartInSec = (currentTime.time - model.startTime.time) / 1000
-                    val timeStartX = timeStartInSec * pixelsPerSec
-                    drawLine(
-                        color = if (isDarkTheme) Color.White else Color.Black,
-                        start = Offset(x = timeStartX, y = canvasHeightOuter),
-                        end = Offset(x = timeStartX, y = 0f),
-                        strokeWidth = 5f,
-                        pathEffect = PathEffect.dashPathEffect(
-                            floatArrayOf(20f, 10f)
-                        )
+                    drawIngestion(
+                        ingestionDrawable = ingestionDrawable,
+                        isDarkTheme = isDarkTheme,
+                        pixelsPerSec = pixelsPerSec,
+                        strokeWidth = strokeWidth,
+                        canvasHeightOuter = canvasHeightOuter
                     )
                 }
-            }
-            drawContext.canvas.nativeCanvas.apply {
-                val fullHours = model.axisDrawable.getFullHours(
+                drawCurrentTime(
+                    startTime = model.startTime,
+                    timelineWidth = model.width,
+                    currentTime = currentTime,
                     pixelsPerSec = pixelsPerSec,
-                    widthInPixels = canvasWidth
+                    isDarkTheme = isDarkTheme,
+                    canvasHeightOuter = canvasHeightOuter
                 )
-                fullHours.forEach { fullHour ->
-                    drawText(
-                        fullHour.label,
-                        fullHour.distanceFromStart,
-                        canvasWithLabelsHeight,
-                        textPaint
-                    )
-                }
             }
+            drawAxis(
+                axisDrawable = model.axisDrawable,
+                pixelsPerSec = pixelsPerSec,
+                canvasWidth = canvasWidth,
+                canvasHeight = canvasWithLabelsHeight,
+                textPaint = textPaint
+            )
+        }
+    }
+}
+
+fun DrawScope.drawIngestion(
+    ingestionDrawable: IngestionDrawable,
+    isDarkTheme: Boolean,
+    pixelsPerSec: Float,
+    strokeWidth: Float,
+    canvasHeightOuter: Float
+) {
+    val color = ingestionDrawable.color.getComposeColor(isDarkTheme)
+    val startX =
+        ingestionDrawable.ingestionPointDistanceFromStart.inWholeSeconds * pixelsPerSec
+    val verticalInsetForLine = strokeWidth / 2
+    inset(vertical = verticalInsetForLine) {
+        val canvasHeightInner = size.height
+        drawCircle(
+            color = color,
+            radius = 10f,
+            center = Offset(x = startX, y = canvasHeightInner)
+        )
+    }
+    ingestionDrawable.timelineDrawable?.let { timelineDrawable ->
+        inset(vertical = verticalInsetForLine) {
+            val canvasHeightInner = size.height
+            drawPath(
+                path = timelineDrawable.getStrokePath(
+                    pixelsPerSec = pixelsPerSec,
+                    height = canvasHeightInner,
+                    startX = startX
+                ),
+                color = color,
+                style = Stroke(
+                    width = strokeWidth,
+                    cap = StrokeCap.Round,
+                    pathEffect = if (timelineDrawable.isDotted) PathEffect.dashPathEffect(
+                        floatArrayOf(20f, 30f)
+                    ) else null
+                )
+            )
+        }
+        drawPath(
+            path = timelineDrawable.getFillPath(
+                pixelsPerSec = pixelsPerSec,
+                height = canvasHeightOuter,
+                startX = startX
+            ),
+            color = color.copy(alpha = 0.1f)
+        )
+    }
+}
+
+fun DrawScope.drawCurrentTime(
+    startTime: Date,
+    timelineWidth: Duration,
+    currentTime: Date,
+    pixelsPerSec: Float,
+    isDarkTheme: Boolean,
+    canvasHeightOuter: Float
+) {
+    val endTime = Date(startTime.time + timelineWidth.inWholeMilliseconds)
+    if (startTime.before(currentTime) && endTime.after(currentTime)) {
+        val timeStartInSec = (currentTime.time - startTime.time) / 1000
+        val timeStartX = timeStartInSec * pixelsPerSec
+        drawLine(
+            color = if (isDarkTheme) Color.White else Color.Black,
+            start = Offset(x = timeStartX, y = canvasHeightOuter),
+            end = Offset(x = timeStartX, y = 0f),
+            strokeWidth = 5f,
+            pathEffect = PathEffect.dashPathEffect(
+                floatArrayOf(20f, 10f)
+            )
+        )
+    }
+}
+
+fun DrawScope.drawAxis(
+    axisDrawable: AxisDrawable,
+    pixelsPerSec: Float,
+    canvasWidth: Float,
+    canvasHeight: Float,
+    textPaint: Paint
+) {
+    val fullHours = axisDrawable.getFullHours(
+        pixelsPerSec = pixelsPerSec,
+        widthInPixels = canvasWidth
+    )
+    drawContext.canvas.nativeCanvas.apply {
+        fullHours.forEach { fullHour ->
+            drawText(
+                fullHour.label,
+                fullHour.distanceFromStart,
+                canvasHeight,
+                textPaint
+            )
         }
     }
 }
