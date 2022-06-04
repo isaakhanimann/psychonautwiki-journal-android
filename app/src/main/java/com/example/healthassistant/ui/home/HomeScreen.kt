cf6807a16b00be522755f82ad154f263dffb8f8c
Isaak <isaak.hanimann@gmail.com>
2022-06-04 17:36:40 +0200

    Make HomeScreen previewable and add dropdown menu to topappbar

--- app/src/main/java/com/example/healthassistant/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/healthassistant/ui/home/HomeScreen.kt
@@ -1,12 +1,20 @@
 package com.example.healthassistant.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.healthassistant.data.experiences.entities.ExperienceWithIngestions
 
 @Composable
 fun HomeScreen(
@@ -15,8 +23,73 @@ fun HomeScreen(
     navigateToEditExperienceScreen: (experienceId: Int) -> Unit,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
+    HomeScreen(
+        navigateToAddExperience = navigateToAddExperience,
+        navigateToExperiencePopNothing = navigateToExperiencePopNothing,
+        navigateToEditExperienceScreen = navigateToEditExperienceScreen,
+        groupedExperiences = homeViewModel.experiencesGrouped.collectAsState().value,
+        deleteExperience = homeViewModel::deleteExperienceWithIngestions
+    )
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(
+        navigateToAddExperience = {},
+        navigateToExperiencePopNothing = {},
+        navigateToEditExperienceScreen = {},
+        groupedExperiences = emptyMap(),
+        deleteExperience = {}
+    )
+}
+
+@Composable
+fun HomeScreen(
+    navigateToAddExperience: () -> Unit,
+    navigateToExperiencePopNothing: (experienceId: Int) -> Unit,
+    navigateToEditExperienceScreen: (experienceId: Int) -> Unit,
+    groupedExperiences: Map<String, List<ExperienceWithIngestions>>,
+    deleteExperience: (ExperienceWithIngestions) -> Unit
+) {
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Experiences") }) },
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Experiences") },
+                actions = {
+                    var isExpanded by remember { mutableStateOf(false) }
+                    Box(
+                        modifier = Modifier
+                            .wrapContentSize(Alignment.TopEnd)
+                    ) {
+                        IconButton(
+                            onClick = { isExpanded = true },
+                        ) {
+                            Icon(Icons.Default.Favorite, contentDescription = "Filter")
+                        }
+                        DropdownMenu(
+                            expanded = isExpanded,
+                            onDismissRequest = { isExpanded = false }
+                        ) {
+                            DropdownMenuItem(
+                                onClick = {
+                                    isExpanded = false
+                                }
+                            ) {
+                                Text("Delete")
+                            }
+                            DropdownMenuItem(
+                                onClick = {
+                                    isExpanded = false
+                                }
+                            ) {
+                                Text("Edit")
+                            }
+                        }
+                    }
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = navigateToAddExperience) {
                 Icon(Icons.Default.Add, "Add New Experience")
@@ -24,24 +97,28 @@ fun HomeScreen(
         }
     ) {
         ExperiencesList(
-            homeViewModel = homeViewModel,
+            groupedExperiences = groupedExperiences,
             navigateToExperiencePopNothing = navigateToExperiencePopNothing,
             navigateToEditExperienceScreen = navigateToEditExperienceScreen,
-            navigateToAddExperience = navigateToAddExperience
+            navigateToAddExperience = navigateToAddExperience,
+            deleteExperience = deleteExperience
         )
     }
 }
 
 @Composable
 fun ExperiencesList(
-    homeViewModel: HomeViewModel,
+    groupedExperiences: Map<String, List<ExperienceWithIngestions>>,
     navigateToExperiencePopNothing: (experienceId: Int) -> Unit,
     navigateToEditExperienceScreen: (experienceId: Int) -> Unit,
     navigateToAddExperience: () -> Unit,
+    deleteExperience: (ExperienceWithIngestions) -> Unit
 ) {
-    val groupedExperiences = homeViewModel.experiencesGrouped.collectAsState().value
     if (groupedExperiences.isEmpty()) {
-        Button(onClick = navigateToAddExperience) {
+        Button(
+            onClick = navigateToAddExperience,
+            modifier = Modifier.padding(10.dp)
+        ) {
             Text("Add Your First Experience")
         }
     } else {
@@ -51,17 +128,17 @@ fun ExperiencesList(
                     SectionTitle(title = year)
                 }
                 items(experiencesInYear.size) { i ->
-                    val expAndIngs = experiencesInYear[i]
+                    val experienceWithIngestions = experiencesInYear[i]
                     ExperienceRow(
-                        expAndIngs,
+                        experienceWithIngestions,
                         navigateToExperienceScreen = {
-                            navigateToExperiencePopNothing(expAndIngs.experience.id)
+                            navigateToExperiencePopNothing(experienceWithIngestions.experience.id)
                         },
                         navigateToEditExperienceScreen = {
-                            navigateToEditExperienceScreen(expAndIngs.experience.id)
+                            navigateToEditExperienceScreen(experienceWithIngestions.experience.id)
                         },
                         deleteExperienceWithIngestions = {
-                            homeViewModel.deleteExperienceWithIngestions(expAndIngs)
+                            deleteExperience(experienceWithIngestions)
                         }
                     )
                     if (i < experiencesInYear.size) {
