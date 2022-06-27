16b635928b06f73bdd925e8468401665d0bba038
Isaak <isaak.hanimann@gmail.com>
2022-06-27 08:32:50 +0200

    Fix IngestionsScreenPreviewProvider

--- app/src/main/java/com/example/healthassistant/ui/previewproviders/IngestionsScreenPreviewProvider.kt
+++ app/src/main/java/com/example/healthassistant/ui/previewproviders/IngestionsScreenPreviewProvider.kt
@@ -2,118 +2,241 @@ package com.example.healthassistant.ui.previewproviders
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.example.healthassistant.data.room.experiences.entities.Ingestion
+import com.example.healthassistant.data.room.experiences.entities.SubstanceColor
+import com.example.healthassistant.data.room.experiences.entities.SubstanceCompanion
+import com.example.healthassistant.data.room.experiences.relations.IngestionWithCompanion
 import com.example.healthassistant.data.substances.AdministrationRoute
 
-class IngestionsScreenPreviewProvider : PreviewParameterProvider<Map<String, List<Ingestion>>> {
-    override val values: Sequence<Map<String, List<Ingestion>>> = sequenceOf(
+class IngestionsScreenPreviewProvider :
+    PreviewParameterProvider<Map<String, List<IngestionWithCompanion>>> {
+    override val values: Sequence<Map<String, List<IngestionWithCompanion>>> = sequenceOf(
         mapOf(
-            // todo: add color
             Pair(
                 first = "2022", second = listOf(
-                    Ingestion(
-                        substanceName = "MDMA",
-                        time = getDate(year = 2022, month = 7, day = 5, hourOfDay = 14, minute = 20)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = 90.0,
-                        isDoseAnEstimate = false,
-                        units = "mg",
-                        experienceId = 0,
-                        notes = "This is one note"
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "MDMA",
+                            time = getDate(
+                                year = 2022,
+                                month = 7,
+                                day = 5,
+                                hourOfDay = 14,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = 90.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note"
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "MDMA",
+                            color = SubstanceColor.PINK
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2022, month = 7, day = 5, hourOfDay = 12, minute = 30)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = null,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = null
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2022,
+                                month = 7,
+                                day = 5,
+                                hourOfDay = 12,
+                                minute = 30
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = null,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2022, month = 6, day = 3, hourOfDay = 9, minute = 5)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = null,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = "Note"
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2022,
+                                month = 6,
+                                day = 3,
+                                hourOfDay = 9,
+                                minute = 5
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = null,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = "Note"
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2022, month = 3, day = 5, hourOfDay = 11, minute = 20)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = null,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = null
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2022,
+                                month = 3,
+                                day = 5,
+                                hourOfDay = 11,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = null,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2022, month = 1, day = 5, hourOfDay = 23, minute = 32)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = null,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = null
-                    )
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2022,
+                                month = 1,
+                                day = 5,
+                                hourOfDay = 23,
+                                minute = 32
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = null,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
+                    ),
                 )
             ),
             Pair(
                 first = "2021", second = listOf(
-                    Ingestion(
-                        substanceName = "MDMA",
-                        time = getDate(year = 2021, month = 7, day = 5, hourOfDay = 14, minute = 20)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = 90.0,
-                        isDoseAnEstimate = false,
-                        units = "mg",
-                        experienceId = 0,
-                        notes = "This is one note"
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "MDMA",
+                            time = getDate(
+                                year = 2021,
+                                month = 7,
+                                day = 5,
+                                hourOfDay = 14,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = 90.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note"
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "MDMA",
+                            color = SubstanceColor.PINK
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2021, month = 7, day = 5, hourOfDay = 12, minute = 30)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = null,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = null
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2021,
+                                month = 7,
+                                day = 5,
+                                hourOfDay = 12,
+                                minute = 30
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = null,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "Ketamine",
-                        time = getDate(year = 2021, month = 6, day = 3, hourOfDay = 9, minute = 5)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = 80.0,
-                        isDoseAnEstimate = true,
-                        units = "mg",
-                        experienceId = 0,
-                        notes = "Note"
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "Ketamine",
+                            time = getDate(
+                                year = 2021,
+                                month = 6,
+                                day = 3,
+                                hourOfDay = 9,
+                                minute = 5
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = 80.0,
+                            isDoseAnEstimate = true,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "Note"
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "Ketamine",
+                            color = SubstanceColor.MINT
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "Cocaine",
-                        time = getDate(year = 2021, month = 3, day = 5, hourOfDay = 11, minute = 20)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = 50.0,
-                        isDoseAnEstimate = false,
-                        units = "mg",
-                        experienceId = 0,
-                        notes = null
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "Cocaine",
+                            time = getDate(
+                                year = 2021,
+                                month = 3,
+                                day = 5,
+                                hourOfDay = 11,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = 50.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "Cocaine",
+                            color = SubstanceColor.BLUE
+                        )
                     ),
-                    Ingestion(
-                        substanceName = "LSD",
-                        time = getDate(year = 2021, month = 1, day = 5, hourOfDay = 23, minute = 32)!!,
-                        administrationRoute = AdministrationRoute.ORAL,
-                        dose = 100.0,
-                        isDoseAnEstimate = false,
-                        units = "µg",
-                        experienceId = 0,
-                        notes = null
-                    )
+                    IngestionWithCompanion(
+                        ingestion = Ingestion(
+                            substanceName = "LSD",
+                            time = getDate(
+                                year = 2021,
+                                month = 1,
+                                day = 5,
+                                hourOfDay = 23,
+                                minute = 32
+                            )!!,
+                            administrationRoute = AdministrationRoute.ORAL,
+                            dose = 100.0,
+                            isDoseAnEstimate = false,
+                            units = "µg",
+                            experienceId = 0,
+                            notes = null
+                        ),
+                        substanceCompanion = SubstanceCompanion(
+                            substanceName = "LSD",
+                            color = SubstanceColor.PURPLE
+                        )
+                    ),
                 )
             )
         ),
