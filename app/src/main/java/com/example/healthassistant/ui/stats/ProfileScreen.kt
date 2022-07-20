ca4124ed999a5ed3ce85bce8e2112f0430599f11
Isaak <isaak.hanimann@gmail.com>
2022-07-20 20:53:36 +0200

    Handle empty profile screen on startup

--- app/src/main/java/com/example/healthassistant/ui/stats/ProfileScreen.kt
+++ app/src/main/java/com/example/healthassistant/ui/stats/ProfileScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -68,35 +69,46 @@ fun ProfileScreen(
             )
         }
     ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(substancesLastUsed.size) { i ->
-                val subStat = substancesLastUsed[i]
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            navigateToSubstanceCompanion(subStat.substanceName)
-                        }
-                        .padding(horizontal = 10.dp, vertical = 5.dp)
-                ) {
-                    val isDarkTheme = isSystemInDarkTheme()
-                    Surface(
-                        shape = CircleShape,
-                        color = subStat.color.getComposeColor(isDarkTheme),
-                        modifier = Modifier.size(25.dp)
-                    ) {}
-                    Column {
-                        Text(text = subStat.substanceName, style = MaterialTheme.typography.h6)
-                        Text(text = "Last used ${subStat.lastUsedText} ago")
-                    }
+        if (substancesLastUsed.isEmpty()) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(text = "Nothing to Show Yet")
                 }
-                if (i < substancesLastUsed.size) {
-                    Divider()
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(substancesLastUsed.size) { i ->
+                    val subStat = substancesLastUsed[i]
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                navigateToSubstanceCompanion(subStat.substanceName)
+                            }
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                    ) {
+                        val isDarkTheme = isSystemInDarkTheme()
+                        Surface(
+                            shape = CircleShape,
+                            color = subStat.color.getComposeColor(isDarkTheme),
+                            modifier = Modifier.size(25.dp)
+                        ) {}
+                        Column {
+                            Text(text = subStat.substanceName, style = MaterialTheme.typography.h6)
+                            Text(text = "Last used ${subStat.lastUsedText} ago")
+                        }
+                    }
+                    if (i < substancesLastUsed.size) {
+                        Divider()
+                    }
                 }
             }
         }
