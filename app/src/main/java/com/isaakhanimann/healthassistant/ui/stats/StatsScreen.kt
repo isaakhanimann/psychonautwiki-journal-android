592fe66800f349774152341994a62f1da25ae3ea
Isaak <isaak.hanimann@gmail.com>
2022-09-21 15:24:53 +0200

    Improve chart

--- app/src/main/java/com/isaakhanimann/healthassistant/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/isaakhanimann/healthassistant/ui/stats/StatsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -86,6 +85,7 @@ fun StatsScreen(
                     style = MaterialTheme.typography.h6,
                     modifier = Modifier.padding(10.dp)
                 )
+                val isDarkTheme = isSystemInDarkTheme()
                 Canvas(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -97,14 +97,22 @@ fun StatsScreen(
                     val spaceBetween = 8f
                     val numSpaces = numBuckets - 1
                     val bucketWidth = (canvasWidth - (numSpaces * spaceBetween)) / numBuckets
+                    val maxCount = statsModel.chartBuckets.maxOf { bucket ->
+                        bucket.sumOf { it.count }
+                    }
                     statsModel.chartBuckets.forEachIndexed { index, colorCounts ->
                         val x = (index * (bucketWidth + spaceBetween)) + (bucketWidth / 2)
-                        drawLine(
-                            color = Color.Blue,
-                            start = Offset(x = x, y = canvasHeight),
-                            end = Offset(x = x, y = 0f),
-                            strokeWidth = bucketWidth,
-                        )
+                        var yStart = canvasHeight
+                        colorCounts.forEach { colorCount ->
+                            val yEnd = canvasHeight - (colorCount.count * canvasHeight / maxCount)
+                            drawLine(
+                                color = colorCount.color.getComposeColor(isDarkTheme),
+                                start = Offset(x = x, y = yStart),
+                                end = Offset(x = x, y = yEnd),
+                                strokeWidth = bucketWidth,
+                            )
+                            yStart = yEnd
+                        }
                     }
                 }
                 Divider()
