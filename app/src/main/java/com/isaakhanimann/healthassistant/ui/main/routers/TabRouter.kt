2bc409ac3377ab1e0f0d30ee1b0155945301f5c7
Isaak <isaak.hanimann@gmail.com>
2022-09-21 11:01:04 +0200

    Better tab icons for experiences and ingestions

--- app/src/main/java/com/isaakhanimann/healthassistant/ui/main/routers/TabRouter.kt
+++ app/src/main/java/com/isaakhanimann/healthassistant/ui/main/routers/TabRouter.kt
@@ -7,8 +7,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.isaakhanimann.healthassistant.R
 
 sealed class TabRouter(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
-    object Experiences : TabRouter("experiences", R.string.experiences, Icons.Filled.LocalLibrary)
-    object Ingestions : TabRouter("ingestions", R.string.ingestions, Icons.Filled.History)
+    object Experiences : TabRouter("experiences", R.string.experiences, Icons.Filled.PieChart)
+    object Ingestions : TabRouter("ingestions", R.string.ingestions, Icons.Filled.Circle)
     object Statistics: TabRouter("statistics", R.string.stats, Icons.Filled.BarChart)
     object Search : TabRouter("search", R.string.search, Icons.Filled.Search)
     object SaferUse : TabRouter("saferUse", R.string.safer, Icons.Filled.HealthAndSafety)
