a6faa9198b681f142ead84cc9c4f659ed4d38377
Isaak <isaak.hanimann@gmail.com>
2022-09-22 15:32:25 +0200

    Comment out updating substances

--- app/src/main/java/com/isaakhanimann/healthassistant/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/isaakhanimann/healthassistant/ui/settings/SettingsScreen.kt
@@ -1,20 +1,18 @@
 package com.isaakhanimann.healthassistant.ui.settings
 
-import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.Launch
-import androidx.compose.material.icons.filled.Restore
-import androidx.compose.material.icons.filled.Update
 import androidx.compose.material.icons.outlined.ContactSupport
 import androidx.compose.material.icons.outlined.QuestionAnswer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,54 +59,54 @@ fun SettingsScreen(
         )
     }) {
         Column(modifier = Modifier.padding(10.dp)) {
-            Text(text = "Last Substance Update", style = MaterialTheme.typography.caption)
-            Text(date, style = MaterialTheme.typography.h6)
-            val context = LocalContext.current
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                TextButton(
-                    onClick = {
-                        updateSubstances(
-                            {
-                                Toast.makeText(
-                                    context, "Update Success",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            },
-                            {
-                                Toast.makeText(
-                                    context, "Try Again Later",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        )
-                    },
-                    enabled = !isUpdating
-                ) {
-                    Icon(
-                        Icons.Filled.Update,
-                        contentDescription = "Update Substances",
-                        modifier = Modifier.size(ButtonDefaults.IconSize)
-                    )
-                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                    Text("Update")
-                }
-                TextButton(
-                    onClick = resetSubstances,
-                    enabled = !isUpdating
-                ) {
-                    Icon(
-                        Icons.Filled.Restore,
-                        contentDescription = "Reset Substances",
-                        modifier = Modifier.size(ButtonDefaults.IconSize)
-                    )
-                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                    Text("Reset")
-                }
-                if (isUpdating) {
-                    CircularProgressIndicator()
-                }
-            }
-            Divider()
+//            Text(text = "Last Substance Update", style = MaterialTheme.typography.caption)
+//            Text(date, style = MaterialTheme.typography.h6)
+//            val context = LocalContext.current
+//            Row(verticalAlignment = Alignment.CenterVertically) {
+//                TextButton(
+//                    onClick = {
+//                        updateSubstances(
+//                            {
+//                                Toast.makeText(
+//                                    context, "Update Success",
+//                                    Toast.LENGTH_SHORT
+//                                ).show()
+//                            },
+//                            {
+//                                Toast.makeText(
+//                                    context, "Try Again Later",
+//                                    Toast.LENGTH_SHORT
+//                                ).show()
+//                            }
+//                        )
+//                    },
+//                    enabled = !isUpdating
+//                ) {
+//                    Icon(
+//                        Icons.Filled.Update,
+//                        contentDescription = "Update Substances",
+//                        modifier = Modifier.size(ButtonDefaults.IconSize)
+//                    )
+//                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+//                    Text("Update")
+//                }
+//                TextButton(
+//                    onClick = resetSubstances,
+//                    enabled = !isUpdating
+//                ) {
+//                    Icon(
+//                        Icons.Filled.Restore,
+//                        contentDescription = "Reset Substances",
+//                        modifier = Modifier.size(ButtonDefaults.IconSize)
+//                    )
+//                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+//                    Text("Reset")
+//                }
+//                if (isUpdating) {
+//                    CircularProgressIndicator()
+//                }
+//            }
+//            Divider()
             val uriHandler = LocalUriHandler.current
             TextButton(
                 onClick = {
