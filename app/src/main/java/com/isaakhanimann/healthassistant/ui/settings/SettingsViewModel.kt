94fa35c7d77af69385a0c56c58ef64eb6e780f36
Isaak <isaak.hanimann@gmail.com>
2022-10-26 06:38:25 +0200

    Remove redundant date formatter

--- app/src/main/java/com/isaakhanimann/healthassistant/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/isaakhanimann/healthassistant/ui/settings/SettingsViewModel.kt
@@ -5,18 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.isaakhanimann.healthassistant.data.room.experiences.ExperienceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
-val Instant.asTextWithDateAndTime: String
-    get() {
-        val dateTime = LocalDateTime.ofInstant(this, ZoneId.systemDefault())
-        val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm")
-        return dateTime.format(formatter)
-    }
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
