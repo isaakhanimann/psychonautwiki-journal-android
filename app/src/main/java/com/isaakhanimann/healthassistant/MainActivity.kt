5a2919f15d7c558766a8eaef1e22a5d488c35738
Isaak <isaak.hanimann@gmail.com>
2022-11-04 12:22:09 +0100

    Don't change system bar colors

--- app/src/main/java/com/isaakhanimann/healthassistant/MainActivity.kt
+++ app/src/main/java/com/isaakhanimann/healthassistant/MainActivity.kt
@@ -3,13 +3,10 @@ package com.isaakhanimann.healthassistant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.isaakhanimann.healthassistant.ui.main.MainScreen
 import com.isaakhanimann.healthassistant.ui.theme.HealthAssistantTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,16 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HealthAssistantTheme {
-                val systemUiController = rememberSystemUiController()
-                val useDarkIcons = !isSystemInDarkTheme()
-                val color = MaterialTheme.colorScheme.surface
-                DisposableEffect(systemUiController, useDarkIcons) {
-                    systemUiController.setSystemBarsColor(
-                        color = color,
-                        darkIcons = useDarkIcons
-                    )
-                    onDispose {}
-                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
