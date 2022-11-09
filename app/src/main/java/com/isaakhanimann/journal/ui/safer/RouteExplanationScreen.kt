12dfa51606de03d7b2a3b79b7194134ddca307df
Isaak <isaak.hanimann@gmail.com>
2022-11-09 18:44:16 +0100

    Style route explanation screen

--- app/src/main/java/com/isaakhanimann/journal/ui/safer/RouteExplanationScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/safer/RouteExplanationScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.isaakhanimann.journal.data.substances.AdministrationRoute
+import com.isaakhanimann.journal.ui.search.substance.CollapsibleSection
 import com.isaakhanimann.journal.ui.search.substance.SectionText
 import com.isaakhanimann.journal.ui.search.substance.VerticalSpace
 import com.isaakhanimann.journal.ui.theme.horizontalPadding
@@ -49,34 +50,41 @@ fun RouteExplanationScreen(navigateToURL: (url: String) -> Unit) {
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .padding(padding)
-                .padding(horizontal = horizontalPadding)
         ) {
-            SectionText(
-                text = """A route of administration is the method in which a psychoactive substance is delivered into the body.
+            Card(modifier = Modifier.padding(horizontal = horizontalPadding, vertical = 3.dp)) {
+                Column(modifier = Modifier.padding(horizontal = horizontalPadding)) {
+                    SectionText(
+                        text = """A route of administration is the method in which a psychoactive substance is delivered into the body.
 The route through which a substance is administered can greatly impact its potency, duration, and subjective effects. For example, many substances are more effective when consumed using particular routes of administration, while some substances are completely inactive with certain routes.
 Determining an optimal route of administration is highly dependent on the substance consumed, its desired duration and potency and side effects, and one's personal comfort level."""
-            )
+                    )
+                    VerticalSpace()
+                }
+            }
             AdministrationRoute.values().filter { !it.isInjectionMethod }.forEach {
-                Text(text = it.displayText, style = MaterialTheme.typography.titleMedium)
-                SectionText(it.articleText)
-                if (it == AdministrationRoute.RECTAL) {
-                    Button(
-                        onClick = { navigateToURL(AdministrationRoute.saferPluggingArticleURL) },
-                        modifier = Modifier.padding(horizontal = 5.dp)
-                    ) {
-                        Icon(
-                            Icons.Outlined.Article,
-                            contentDescription = "Open Link",
-                        )
-                        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-                        Text("Safer Plugging")
+                CollapsibleSection(title = it.displayText) {
+                    SectionText(it.articleText)
+                    if (it == AdministrationRoute.RECTAL) {
+                        Button(
+                            onClick = { navigateToURL(AdministrationRoute.saferPluggingArticleURL) },
+                            modifier = Modifier.padding(horizontal = 5.dp)
+                        ) {
+                            Icon(
+                                Icons.Outlined.Article,
+                                contentDescription = "Open Link",
+                            )
+                            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                            Text("Safer Plugging")
+                        }
                     }
                     VerticalSpace()
                 }
             }
             AdministrationRoute.values().filter { it.isInjectionMethod }.forEach {
-                Text(text = it.displayText, style = MaterialTheme.typography.titleMedium)
-                SectionText(it.articleText)
+                CollapsibleSection(title = it.displayText) {
+                    SectionText(it.articleText)
+                    VerticalSpace()
+                }
             }
         }
     }
