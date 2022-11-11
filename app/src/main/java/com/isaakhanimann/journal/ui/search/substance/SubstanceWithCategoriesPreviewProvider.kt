0b829a9a5ea32d34b4e56498534cd92972190e11
Isaak <isaak.hanimann@gmail.com>
2022-11-11 14:11:43 +0100

    Add copyright to all files

--- app/src/main/java/com/isaakhanimann/journal/ui/search/substance/SubstanceWithCategoriesPreviewProvider.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/search/substance/SubstanceWithCategoriesPreviewProvider.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 3.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ */
+
 package com.isaakhanimann.journal.ui.search.substance
 
 import androidx.compose.ui.graphics.Color
