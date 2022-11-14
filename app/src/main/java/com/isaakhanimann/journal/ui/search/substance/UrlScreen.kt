b2e3e8633330388e4d4c28b4538927613403d178
Isaak <isaak.hanimann@gmail.com>
2022-11-14 15:01:13 +0100

    Add copy link to urlscreen

--- app/src/main/java/com/isaakhanimann/journal/ui/search/substance/UrlScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/search/substance/UrlScreen.kt
@@ -5,29 +5,48 @@
 
 package com.isaakhanimann.journal.ui.search.substance
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.web.LoadingState
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UrlScreen(url: String) {
-    val state = rememberWebViewState(url = url)
-    Column {
-        val loadingState = state.loadingState
-        if (loadingState is LoadingState.Loading) {
-            LinearProgressIndicator(
-                progress = loadingState.progress,
-                modifier = Modifier.fillMaxWidth()
+    val context = LocalContext.current
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {
+            Toast.makeText(
+                context,
+                "Link Copied",
+                Toast.LENGTH_SHORT
+            ).show()
+        }) {
+            Icon(Icons.Filled.ContentCopy, "Localized description")
+        }
+    }) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            val state = rememberWebViewState(url = url)
+            val loadingState = state.loadingState
+            if (loadingState is LoadingState.Loading) {
+                LinearProgressIndicator(
+                    progress = loadingState.progress,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            WebView(
+                state = state,
+                modifier = Modifier.weight(1f),
             )
         }
-        WebView(
-            state = state,
-            modifier = Modifier.weight(1f),
-        )
     }
 }
