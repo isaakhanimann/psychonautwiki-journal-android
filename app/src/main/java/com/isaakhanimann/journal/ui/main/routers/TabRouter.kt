79e2af606fd6c36e5868c4959e76225a5be8f8c1
Isaak <isaak.hanimann@gmail.com>
2022-11-15 09:25:47 +0100

    Fix search screen layout

--- app/src/main/java/com/isaakhanimann/journal/ui/main/routers/TabRouter.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/main/routers/TabRouter.kt
@@ -6,11 +6,13 @@
 package com.isaakhanimann.journal.ui.main.routers
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.BarChart
 import androidx.compose.material.icons.outlined.HealthAndSafety
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Timeline
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -23,7 +25,7 @@ import com.isaakhanimann.journal.ui.stats.StatsScreen
 
 sealed class TabRouter(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
     object Journal : TabRouter("journal", R.string.journal, Icons.Outlined.Timeline)
-    object Statistics: TabRouter("statistics", R.string.stats, Icons.Outlined.BarChart)
+    object Statistics : TabRouter("statistics", R.string.stats, Icons.Outlined.BarChart)
     object Search : TabRouter("search", R.string.search, Icons.Outlined.Search)
     object SaferUse : TabRouter("saferUse", R.string.safer, Icons.Outlined.HealthAndSafety)
 }
@@ -57,6 +59,7 @@ fun NavGraphBuilder.tabGraph(navController: NavController) {
             },
             onCustomSubstanceTap = navController::navigateToEditCustomSubstance,
             navigateToAddCustomSubstanceScreen = navController::navigateToAddCustom,
+            modifier = Modifier.fillMaxSize()
         )
     }
     tabComposableWithTransitions(
