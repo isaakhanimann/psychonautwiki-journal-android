e1fe961ef39695c5552f492ac3df0622d53f5561
Isaak <isaak.hanimann@gmail.com>
2023-05-16 07:50:01 +0200

    Update version to 6.0

--- app/src/main/java/com/isaakhanimann/journal/ui/Constants.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/Constants.kt
@@ -23,6 +23,6 @@ const val DOSE_DISCLAIMER =
 
 const val VOLUMETRIC_DOSE_ARTICLE_URL = "https://psychonautwiki.org/wiki/Volumetric_liquid_dosing"
 
-const val VERSION_NAME = "5.0"
+const val VERSION_NAME = "6.0"
 
 const val FULL_STOMACH_DISCLAIMER = "A full stomach delays the onset of oral ingestions by approx. 3 hours."
