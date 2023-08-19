dd7a0d5dc098e9cc189bcbe5599213dc57168825
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2023-08-19 14:31:25 +0200

    Revert "Remove tolerance info and ingestions from substance companion screen"
    This reverts commit e89bffe806eb8632578301daa51a02c5b5afbd07.

---
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/stats/substancecompanion/SubstanceCompanionScreenPreviewProvider.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2022-2023. Isaak Hanimann.
+ * This file is part of PsychonautWiki Journal.
+ *
+ * PsychonautWiki Journal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * PsychonautWiki Journal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PsychonautWiki Journal.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
+ */
+
+package com.isaakhanimann.journal.ui.tabs.stats.substancecompanion
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.isaakhanimann.journal.data.room.experiences.entities.*
+import com.isaakhanimann.journal.data.substances.AdministrationRoute
+import com.isaakhanimann.journal.ui.utils.getInstant
+
+class SubstanceCompanionScreenPreviewProvider :
+    PreviewParameterProvider<Pair<SubstanceCompanion, List<IngestionsBurst>>> {
+    override val values: Sequence<Pair<SubstanceCompanion, List<IngestionsBurst>>> = sequenceOf(
+        Pair(
+            first = SubstanceCompanion(
+                substanceName = "Cocaine",
+                color = AdaptiveColor.BLUE
+            ),
+            second = listOf(
+                IngestionsBurst(
+                    timeUntil = "35 min",
+                    experience = Experience(
+                        id = 1,
+                        title = "Vienna Weekend",
+                        creationDate = getInstant(
+                            year = 2022,
+                            month = 7,
+                            day = 20,
+                            hourOfDay = 14,
+                            minute = 20
+                        )!!,
+                        sortDate = getInstant(
+                            year = 2022,
+                            month = 7,
+                            day = 20,
+                            hourOfDay = 14,
+                            minute = 20
+                        )!!,
+                        text = "",
+                        isFavorite = false,
+                        location = null
+                    ),
+                    ingestions = listOf(
+                        Ingestion(
+                            substanceName = "Cocaine",
+                            time = getInstant(
+                                year = 2022,
+                                month = 7,
+                                day = 20,
+                                hourOfDay = 14,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.INSUFFLATED,
+                            dose = 20.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note",
+                            stomachFullness = StomachFullness.EMPTY
+                        ),
+                        Ingestion(
+                            substanceName = "Cocaine",
+                            time = getInstant(
+                                year = 2022,
+                                month = 7,
+                                day = 20,
+                                hourOfDay = 13,
+                                minute = 40
+                            )!!,
+                            administrationRoute = AdministrationRoute.INSUFFLATED,
+                            dose = 30.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note",
+                            stomachFullness = StomachFullness.EMPTY
+                        )
+                    )
+                ),
+                IngestionsBurst(
+                    timeUntil = "2 weeks",
+                    experience = Experience(
+                        id = 1,
+                        title = "21. Birthday",
+                        creationDate = getInstant(
+                            year = 2022,
+                            month = 7,
+                            day = 4,
+                            hourOfDay = 14,
+                            minute = 20
+                        )!!,
+                        sortDate = getInstant(
+                            year = 2022,
+                            month = 7,
+                            day = 4,
+                            hourOfDay = 14,
+                            minute = 20
+                        )!!,
+                        text = "",
+                        isFavorite = false,
+                        location = null
+                    ),
+                    ingestions = listOf(
+                        Ingestion(
+                            substanceName = "Cocaine",
+                            time = getInstant(
+                                year = 2022,
+                                month = 7,
+                                day = 4,
+                                hourOfDay = 14,
+                                minute = 20
+                            )!!,
+                            administrationRoute = AdministrationRoute.INSUFFLATED,
+                            dose = 10.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note",
+                            stomachFullness = StomachFullness.EMPTY
+                        ),
+                        Ingestion(
+                            substanceName = "Cocaine",
+                            time = getInstant(
+                                year = 2022,
+                                month = 7,
+                                day = 4,
+                                hourOfDay = 13,
+                                minute = 40
+                            )!!,
+                            administrationRoute = AdministrationRoute.INSUFFLATED,
+                            dose = 20.0,
+                            isDoseAnEstimate = false,
+                            units = "mg",
+                            experienceId = 0,
+                            notes = "This is one note",
+                            stomachFullness = StomachFullness.EMPTY
+                        )
+                    )
+                )
+            )
+        )
+    )
+}
