32d6d0adcfe1c2546186c5fbb4f0cbf8891ec562
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2024-02-18 08:08:06 +0100

    Make unknown dose button explicit

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/dose/customunit/ChooseDoseCustomUnitScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/dose/customunit/ChooseDoseCustomUnitScreen.kt
@@ -34,18 +34,17 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NavigateNext
-import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -177,16 +176,7 @@ fun ChooseDoseCustomUnitScreen(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("${customUnit.substanceName} (${customUnit.name})") },
-                actions = {
-                    IconButton(onClick = useUnknownDoseAndNavigate) {
-                        Icon(
-                            imageVector = Icons.Default.QuestionMark,
-                            contentDescription = "Log Unknown Dose"
-                        )
-                    }
-                })
+            TopAppBar(title = { Text("${customUnit.substanceName} (${customUnit.name})") })
         },
         floatingActionButton = {
             if (isValidDose) {
@@ -314,6 +304,9 @@ fun ChooseDoseCustomUnitScreen(
                         )
                     }
                 }
+            }
+            TextButton(onClick = useUnknownDoseAndNavigate) {
+                Text(text = "Log unknown dose")
             }
         }
     }
