e89235f11675d3e8642f2801209f0fcc056adac1
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2024-10-15 07:03:46 +0200

    Add color button content description

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/time/ColorPicker.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/addingestion/time/ColorPicker.kt
@@ -47,6 +47,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -189,6 +191,7 @@ fun CircleColorButtons(
                     .clickable(
                         onClick = { onTapOnColor(color) }
                     )
+                    .semantics { contentDescription = color.name }
             ) {}
         }
     }
