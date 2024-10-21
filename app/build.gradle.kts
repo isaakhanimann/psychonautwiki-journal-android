067676847c185b856ee761bc3eb7a119597842a2
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2024-10-21 22:53:35 +0200

    Move id("kotlin-kapt") down in plugins section to get rid of build warning
    https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("androidx.room")
+    id("kotlin-kapt")
 }
 
 android {
