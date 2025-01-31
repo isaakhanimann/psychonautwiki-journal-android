1e6b6247c2375a6c887e38168529866a851d7db7
Isaak <31287934+isaakhanimann@users.noreply.github.com>
2025-01-31 14:11:13 +0100

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.isaakhanimann.journal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 57
-        versionName = "11.6"
+        versionCode = 58
+        versionName = "11.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
